bd40899482a2ce45f57791a3b47ca5c92f3348d9
agadzinski93 <agadzinski93@users.noreply.github.com>
2023-12-22 22:57:33 -0800

    Fixed Cloudinary

--- utilities/cloudinary.mjs
+++ utilities/cloudinary.mjs
@@ -1,17 +1,16 @@
 //const cloudinary = require('cloudinary').v2;
 import cloudinary from 'cloudinary';
 const cloudinaryV2 = cloudinary.v2;
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import {CloudinaryStorage} from 'multer-storage-cloudinary';
 
-cloudinary.config({
+cloudinaryV2.config({
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_SECRET,
-    secure:true,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary,
+    cloudinary:cloudinaryV2,
     params: {
         folder:'Programminghelp',
     }
