b9d8a8c51549b2a577dd58fea08ff25241d127ab
agadzinski93 <agadzinski93@users.noreply.github.com>
2024-06-05 16:03:17 -0700

    Updated Babel-Jest Transform Props

--- server/utilities/validators/topicValidator.ts
+++ server/utilities/validators/topicValidator.ts
@@ -2,12 +2,12 @@ import Joi from "joi";
 
 const topicValidator = Joi.object({
   name: Joi.string().min(3).max(45)
-    .regex(/[-\\\<\>]/, {invert:true})
+    .regex(/[-\\\<\>]/, { invert: true })
     .required().messages({
-    "string.min": "Title must be at least 3 characters",
-    "string.max": "Title must be no more than 45 characters",
-    "string.pattern.invert.base": "Topic name cannot include -,\\,<,>"
-  }),
+      "string.min": "Title must be at least 3 characters",
+      "string.max": "Title must be no more than 45 characters",
+      "string.pattern.invert.base": "Topic name cannot include -,\\,<,>"
+    }),
   description: Joi.string().min(3).max(512).required().messages({
     "string.min": "Description must be at least 3 characters",
     "string.max": "Description must be no more than 512 characters"
@@ -16,7 +16,7 @@ const topicValidator = Joi.object({
     .required()
     .valid("Beginner", "Intermediate", "Advanced")
     .messages({ "string.base": "Difficulty must be text" })
-  
+
 });
 
 export default topicValidator;
